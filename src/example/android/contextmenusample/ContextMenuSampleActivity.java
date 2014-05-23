8fe9985c98e8e55a80fed98207dbff5e50699b2f
i0001 <il0venature1008@gmail.com>
2014-05-23 11:28:15 +0900

    Sample of ContextMenu

---
+++ src/example/android/contextmenusample/ContextMenuSampleActivity.java
@@ -0,0 +1,22 @@
+package example.android.contextmenusample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ContextMenuSampleActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_context_menu_sample);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_context_menu_sample, menu);
+		return true;
+	}
+
+}
